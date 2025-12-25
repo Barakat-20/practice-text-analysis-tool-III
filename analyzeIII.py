e59c5f97201395472cf5e4e5b3466ce614d505b6
Barakat Egberinde <barakategberinde@gmail.com>
2025-12-25 07:41:56 +0100

    Got user input

---
+++ analyzeIII.py
@@ -0,0 +1,13 @@
+print("\nWelcome to the text analysis tool")
+
+#Get Username
+
+#Print message prompting user to enter username 
+
+print("\nEnter your name:")
+
+# Get user input
+username = input("\n")
+
+# Greet user
+print("Hello, " + username)
